37a2e37035618d776da8930d08668a234b2c4be0
lucassampaioleite <lucas.sampaio.leite@gmail.com>
2023-02-03 14:26:30 -0300

    CRUD Spring boot with jpa, hibernate and mysql

---
+++ 03_CRUD_spring_boot_jpa_hibernate_mysql/rest-with-spring-boot-and-java-lucas/src/main/java/leite/sampaio/lucas/controllers/PersonController.java
@@ -0,0 +1,57 @@
+package leite.sampaio.lucas.controllers;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import leite.sampaio.lucas.model.Person;
+import leite.sampaio.lucas.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices service;
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findByID() throws Exception{
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}", 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findByID(@PathVariable(value="id") Long id){
+		return service.findById(id);
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person){
+		return service.create(person);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person){
+		return service.create(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value="id") Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ 03_CRUD_spring_boot_jpa_hibernate_mysql/rest-with-spring-boot-and-java-lucas/src/test/java/leite/sampaio/lucas/StartupTests.java
@@ -0,0 +1,13 @@
+package leite.sampaio.lucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
